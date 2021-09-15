7033426488ca36723d5ce83487dc8d7d9639c98e
Donald Oakes <donald.oakes@centurylink.com>
2021-09-15 12:30:31 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# cosmos-db-emulator-vm-mongodb
+# Provision VM & Access Desktop
+
+### Start VM
+
+`vagrant up`
+
+### Reprovision - reboot & run provision.ps1 again
+
+`vagrant reload --provision`
+
+### RDP to the VM
+
+`xfreerdp /cert-ignore /u:vagrant /p:vagrant /v:127.0.0.1:33389`
+
+# Azure Comsos Emulator
+
+### Cosmos Web UI
+
+`https://localhost:8081/_explorer/index.html`
+
+### Key
+
+`C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==`
+
+### Screenshots
+
+![alt text](/imgs/az_cosmos_1.jpg)
+
+![alt text](/imgs/az_cosmos_2.jpg)
+
+# Azure Storage Emulator
+
+### Credentials 
+
+```
+Account name: devstoreaccount1
+Account key: Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==
+```
+
+### Endpoints
+
+The service endpoints for the storage emulator are:
+ - Blob service: http://127.0.0.1:10000/<account-name>/<resource-path>
+ - Queue service: http://127.0.0.1:10001/<account-name>/<resource-path>
+ - Table service: http://127.0.0.1:10002/<account-name>/<resource-path>
+
+
+### Connection Strings
+
+ ```
+ DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;
+AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;
+BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;
+TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;
+QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;
+```
---
+++ provision.ps1
@@ -0,0 +1,35 @@
+Write-Output "Intalling Cosmos DB Emulator"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(New-Object System.Net.WebClient).DownloadFile('https://aka.ms/cosmosdb-emulator', 'C:\vagrant\cosmos.msi')
+Start-Process -wait C:\vagrant\cosmos.msi -ArgumentList "/quiet"
+
+Write-Output "Installing NSSM"
+(New-Object System.Net.WebClient).DownloadFile('https://nssm.cc/release/nssm-2.24.zip', 'C:\vagrant\nssm.zip')
+Expand-Archive C:\vagrant\nssm.zip -DestinationPath C:\vagrant\nssm -Force
+
+Write-Output "Installing Cosmos DB Emulator as a Service"
+C:\vagrant\nssm\nssm-2.24\win64\nssm.exe install cosmosdbemulator "C:\vagrant\service_startup.cmd"
+C:\vagrant\nssm\nssm-2.24\win64\nssm.exe set cosmosdbemulator Start SERVICE_DELAYED_AUTO_START
+C:\vagrant\nssm\nssm-2.24\win64\nssm.exe set cosmosdbemulator Type SERVICE_INTERACTIVE_PROCESS
+C:\vagrant\nssm\nssm-2.24\win64\nssm.exe start cosmosdbemulator
+
+Write-Output "Setting Service Failure Actions"
+Set-ItemProperty -Name 'FailureActions' -Path 'HKLM:\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\cosmosdbemulator' -Value ([byte[]](0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x3,0x0,0x0,0x0,0x14,0x0,0x0,0x0,0x1,0x0,0x0,0x0,0x60,0xea,0x0,0x0,0x1,0x0,0x0,0x0,0x60,0xea,0x0,0x0,0x1,0x0,0x0,0x0,0x60,0xea,0x0,0x0))
+
+Write-Output "Disabling Windows Firewall"
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+Write-Output "Installing SQLLocalDB"
+(New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SqlLocalDB.MSI', 'C:\vagrant\SqlLocalDB.MSI')
+Start-Process -wait msiexec -ArgumentList "/i","C:\vagrant\SqlLocalDB.MSI","/qn","IACCEPTSQLLOCALDBLICENSETERMS=YES"
+
+Write-Output "Installing Azure Storage Emulator"
+(New-Object System.Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?linkid=717179&clcid=0x409', 'C:\vagrant\az_storage_emulator.msi')
+Start-Process -wait C:\vagrant\az_storage_emulator.msi -ArgumentList "/quiet"
+
+Write-Output "Configuring Azure Storage Emulator to allow remote access"
+$vm_ip = (Get-NetIPAddress -InterfaceAlias "Ethernet" -AddressFamily "IPv4").IPAddress
+$storage_emulator_config_path = "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe.config"
+((Get-Content -path $storage_emulator_config_path -Raw) -replace '127.0.0.1', $vm_ip ) | Set-Content -Path $storage_emulator_config_path
+
+Write-Output "Done"
